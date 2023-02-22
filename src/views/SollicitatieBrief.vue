7003e4858eda8cbe1980784a710746206d48bf85
M-ayash <mohammadhd1987@gmail.com>
2023-02-22 12:19:55 +0100

    SollicitatieBrief fix

--- src/views/SollicitatieBrief.vue
+++ src/views/SollicitatieBrief.vue
@@ -1,98 +1,23 @@
 <template>
-  <div class="CV">
-    <div class="cv-title">
-      <h1 class="left-title">Mohammad</h1>
-      <h1 class="right-title">Ayash</h1>
-    </div>
-    <section class="cv-left">
-      <div class="cv-img">
-        <img src="@/assets/img/main-img.jpg" alt="Mohammad Ayash" class="" />
-        <p class="description p-3">
-          Enthousiaste ICT specialist met een jaar ervaring in front-end ontwikkeling (WordPress en VueJs) en Hardware .opzoek naar een leuke baan waarin ik verder kan ontwikkelen samen met andere ICT collega's .
-        </p>
+  <div class="sollicitatiebrief" contenteditable="true">
+    <section class="peronalia">
+      <div class="personalia-logo">
+        <img src="@/assets/img/logo.jpg" alt="Mohammad Ayash" />
+        <h1>Mohammad Ayash</h1>
       </div>
-      <hr />
-      <div class="activite">
-        <h3>ACTIVITEITEN EN INTERESSES</h3>
-        <ul>
-          <li>Lezen</li>
-          <li>Wandelen</li>
-          <li>voetbal</li>
-          <li>programmeren</li>
-        </ul>
-      </div>
-      <div class="skills">
-        <h3>Talen</h3>
-        <ul>
-          <li>
-            <span>Engels</span>
-            <div class="progress">
-              <span class="progress-bar bg-info" style="width: 85%">85%</span>
-            </div>
-          </li>
-          <li>
-            Nederlands
-            <div class="progress">
-              <span class="progress-bar bg-info" style="width: 75%">75%</span>
-            </div>
-          </li>
-          <li>
-           Arabich
-            <div class="progress">
-              <span class="progress-bar bg-info" style="width: 99%">99%</span>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div class="skills">
-        <h3>VAARDIGHEDEN</h3>
-        <ul>
-          <li>
-            <span>Frontend</span>
-            <div class="progress">
-              <span class="progress-bar bg-info" style="width: 80%">80%</span>
-            </div>
-          </li>
-          <li>
-            VueJs
-            <div class="progress">
-              <span class="progress-bar bg-info" style="width: 75%">75%</span>
-            </div>
-          </li>
-          <li>
-            <span>Backend</span>
-            <div class="progress">
-              <span class="progress-bar bg-info" style="width: 70%">70%</span>
-            </div>
-          </li>
-          <li>
-            Wordpress
-            <div class="progress">
-              <span class="progress-bar bg-info" style="width: 80%">80%</span>
-            </div>
-          </li>
-          <li>
-            Hardware
-            <div class="progress">
-              <span class="progress-bar bg-info" style="width: 60%">60%</span>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </section>
-    <!---->
-    <section class="cv-right p-3">
-      <h3 class="job-name">ICT specialist</h3>
       <div class="personalia-details">
+        <h1>Personalia</h1>
+        <div class="name">
+          <p><i class="fa-solid fa-user"></i> Mohammad Ayash</p>
+        </div>
         <div class="email">
-          <p><i class="fa-solid fa-envelope"></i> mohammadhd1987@gmail.com</p>
+          <p><i class="fa-solid fa-envelope"></i> m.ayash@ictmaatwerk.com</p>
         </div>
         <div class="phone"></div>
         <div class="adress">
-          <p><i class="fa-solid fa-map-location-dot"></i> Tamboer 9</p>
+          <p><span>🏡</span> Tamboer 9</p>
           <p>6866 ED, Heelsum</p>
         </div>
-        <hr />
       </div>
     </section>
     <section class="sollicitatier">
@@ -151,133 +76,66 @@
             goed beeld gekregen van mijn capaciteiten die hiervoor nodig zijn.
           </p>
         </div>
+        <p class="solliciteren-footer">Met vriendelijke groet</p>
+        <p class="name">Mohammad Ayash</p>
+        <p>Bijlage: Curriculum vitae</p>
       </div>
     </section>
   </div>
 </template>
 
 <style>
-.CV {
-  display: block;
+.sollicitatiebrief {
   background-color: #fff;
-  min-width: 100%;
-}
-.cv-title h1 {
-  display: inline-block;
-  width: 50%;
-  font: 4em sans-serif;
-  text-align: center;
-  font-weight: bolder;
-  margin-bottom: 0;
-  background-color: #fff;
-  color: rgb(57, 90, 135);
-}
-.job-name {
-  background-color: rgb(57, 90, 135);
-  color: #fff;
-  text-align: center;
-  margin: 0;
-}
-/*/////////////*/
-.cv-left {
-  width: 40%;
-  float: left;
-  background-color: rgb(57, 90, 135);
-  color: #fff;
-  padding-bottom: 214px;
-}
-.cv-img {
-  width: 100%;
-  text-align: center;
-}
-.cv-img img {
-  border-radius: 50%;
-  border: 3px solid #ddd;
+  min-height: 100vh;
   max-width: 100%;
-  height: 30vh;
 }
-
-.description {
-  font-size: 1.4em;
-  text-align: justify;
+.sollicitatiebrief p {
+  font-family: "Times New Roman", Times, sans-serif;
+  font-size: 1.2em;
 }
-.activite,
-.skills {
-  margin: 20px 10px;
-}
-.activite h3,
-.skills h3 {
-  text-align: center;
-}
-.progress {
-  display: inline-flex;
-  width: 10em;
-  float: right;
-}
-
-/*/ //////////////*/
-
-.cv-right {
-  width: 60%;
+.peronalia {
+  min-height: 100vh;
+  width: 30%;
   float: left;
-  color: rgb(57, 90, 135);
+  overflow-wrap: break-word;
+  background-color: rgb(206, 218, 214);
 }
-
+.personalia-logo {
+  position: relative;
+}
+.personalia-logo img {
+  width: 100%;
+}
+.personalia-logo h1 {
+  text-align: center;
+  position: absolute;
+  top: 10%;
+  left: 10%;
+  color: #fff;
+}
 .personalia-details {
   padding: 20px;
 }
 .personalia-details h1 {
-  color: rgb(57, 90, 135);
+  color: rgb(10, 44, 117);
   padding-bottom: 10px;
   text-decoration: underline;
+}
+.company-details {
+  padding: 0px;
+  margin: 0px;
+  font-size: 13px;
+  line-height: 80%;
+}
+.sollicitatier {
+  width: 70%;
+  float: left;
+  padding: 100px 20px;
 }
 .solliciteren-text {
   text-align: justify;
   white-space: normal;
-}
-
-.work-exper,
-.opleiding {
-  color: #000;
-}
-.work-exper h2,
-.opleiding h2 {
-  text-align: center;
-  color: rgb(57, 90, 135);
-}
-.work-exper h3,
-.opleiding h3 {
-  color: rgb(0, 22, 51);
-}
-.work-exper p,
-.opleiding p {
-  color: gray;
-  padding-left: 10px;
-}
-@media (max-width: 768px) {
-   .CV{
-    padding-top:10em;
-    width: 100%;
-  }
-}
-@media (max-width: 480px) {
-  .cv-left,.cv-right{
-    width: 100%;
-  }
-  .cv-left{
-    padding-bottom: 20px;
-  }
-  .skills ul li{
-    margin-bottom: 5px;
-  }
-}
-@media print {
-  .cv {
-    min-height: 90%;
-    max-width: 100%;
-  }
-  .cv-img img {
-    height: 20vh;
-  }
+  font-size: 13px;
 }
 </style>
